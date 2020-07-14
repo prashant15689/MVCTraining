932eba2d11423304010395deb99ed06ecab32f48
Prashant <prashant15689@outlook.com>
2020-07-15 00:25:45 +0530

    Removed about and contact views and actions.

--- MVCTrainingProject/Controllers/HomeController.cs
+++ MVCTrainingProject/Controllers/HomeController.cs
@@ -1,8 +1,5 @@
 ﻿using MVCTrainingProject.Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace MVCTrainingProject.Controllers
@@ -14,20 +11,6 @@ namespace MVCTrainingProject.Controllers
             MVCTrainingDBEntities dbContext = new MVCTrainingDBEntities();
             IEnumerable<Employees> employees = dbContext.Employees;
             return View(employees);
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "This is an MVC application.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "My contact is - 9560348122.";
-
-            return View();
         }
     }
 }
