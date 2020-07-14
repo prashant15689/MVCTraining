0504ab0b0642cc2b6ea8710d4eac145bea8c9a29
Prashant <prashant15689@outlook.com>
2020-07-15 00:10:58 +0530

    Initial Commit

---
+++ MVCTrainingProject/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using MVCTrainingProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTrainingProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            MVCTrainingDBEntities dbContext = new MVCTrainingDBEntities();
+            IEnumerable<Employees> employees = dbContext.Employees;
+            return View(employees);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
