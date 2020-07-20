11255d9ec622d9ab094876663f2891d5ff13632c
Prashant <prashants2@chetu.com>
2020-07-20 14:19:34 +0530

    Added partial view and used for loop in view to render data.

---
+++ MVCTrainingProject/Models/EmployeeDataLogic.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCTrainingProject.Models
+{
+    public class EmployeeDataLogic
+    {
+        public string Get()
+        {
+            return "Hello";
+        }
+        public string Get(string name)
+        {
+            return name;
+        }
+
+        public string Get(string name, int Id)
+        {
+            return name + " - ID: " + Id.ToString();
+        }
+
+        public void Get(string name, object model)
+        {
+            // ksdj
+        }
+    }
+}
